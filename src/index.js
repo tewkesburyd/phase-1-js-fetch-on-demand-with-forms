f3c017c0929414d3bb0b2a85bdc20967956430fe
David Tewkesbury <tewkesburyd@hotmail.com>
2022-11-04 11:09:42 -0600

    commit tree 50cfd825e33b108d178868e80924b29078faaec1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+
 const init = () => {
-  
+    const form = document.querySelector('form')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const searchValue = document.querySelector('#searchByID')
+        
+        fetch(`http://localhost:3000/movies/${searchValue.value}`)
+        .then(res => res.json())
+        .then(movies => searchResults(movies))
+})
+
 }
 
-document.addEventListener('DOMContentLoaded', init);
+const searchResults = movies => {
+    const title = document.querySelector('#movieDetails h4')
+    const summary = document.querySelector('#movieDetails p')
+    const form = document.querySelector('form')
+
+    title.innerText = movies.title
+    summary.innerText = movies.summary
+    form.reset()
+}
+
+//document.addEventListener('DOMContentLoaded', init);
+init()
